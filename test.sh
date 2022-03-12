6b94e65d0cbd3ccc06f396ff773027492268615f
Conrad Gustafson <c.gustafson@cgi.com>
2022-03-11 16:22:46 -0800

    seven

--- test.sh
+++ test.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 set -eux
+echo "::set-output name=path::scripts"
 declare -a dirs=()
-# for d in scripts/*/ ; do
-    # if [ ${d} == *"${{ steps.files.outputs.all }}"* ];
-    # then
-        #dirs+=("$d")
-    # fi
-# done
-if [ ${#dirs[@]} -gt 1 ];
-then
-    echo "You may not change more than 1 directory in a single PR. This one has ${#dirs[@]} directory changes."
-    exit 1
-fi
-if [ ${#dirs[@]} -eq 0 ];
-then
-    echo "No changes to scripts directories."
-    exit 0
-fi
+for d in scripts/*/ ; do
+# if [ ${d} == *"${{ steps.files.outputs.all }}"* ];
+# then
+    dirs+=("$d")
+# fi
+done
+# if [ ${#dirs[@]} -gt 1 ];
+# then
+# echo "You may not change more than 1 directory in a single PR. This one has ${#dirs[@]} directories with changes."
+# exit 1
+# fi
+echo "::set-output name=path::${dirs[0]}"
